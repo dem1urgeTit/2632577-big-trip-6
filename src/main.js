5b7f6be250e4078a8363ac80b092d49ec2917a08
dem1urgeTit <twentysevenowls@gmail.com>
2026-06-18 20:27:22 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
-import ApiService from './api-service.js';
 import TripModel from './model/trip-model.js';
 import FilterModel from './model/filter-model.js';
 import TripPresenter from './presenter/trip-presenter.js';
-import FilterPresenter from './presenter/filter-presenter.js';
+import FiltersPresenter from './presenter/filter-presenter.js';
+import Api from './api.js';
 
-const END_POINT = 'https://21.objects.pages.academy/big-trip';
-const AUTHORIZATION = 'Basic bigtrip2025superkey';
-const api = new ApiService(END_POINT, AUTHORIZATION);
+const api = new Api();
+
+const tripEventsSection = document.querySelector('.trip-events');
+const tripInfoContainer = document.querySelector('.trip-main__trip-info');
 
 const tripModel = new TripModel(api);
 const filterModel = new FilterModel();
 
 const filtersContainer = document.querySelector('.trip-controls__filters');
-const filterPresenter = new FilterPresenter(filtersContainer, filterModel, tripModel);
-filterPresenter.init();
+const filtersPresenter = new FiltersPresenter(filtersContainer, tripModel);
+filtersPresenter.init();
 
 const tripPresenter = new TripPresenter(tripModel, filterModel);
 tripPresenter.init();
