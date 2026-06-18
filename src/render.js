7c529ceb3f2e4a9430194631c6baea248e030f03
dem1urgeTit <twentysevenowls@gmail.com>
2026-06-18 20:46:06 +0500

    Update render.js

--- src/render.js
+++ src/render.js
@@ -8,6 +8,7 @@ const RenderPosition = {
 function createElement(template) {
   const newElement = document.createElement('div');
   newElement.innerHTML = template;
+
   return newElement.firstElementChild;
 }
 
@@ -15,20 +16,4 @@ function render(component, container, place = RenderPosition.BEFOREEND) {
   container.insertAdjacentElement(place, component.getElement());
 }
 
-function replace(newComponent, oldComponent) {
-  const newElement = newComponent.getElement();
-  const oldElement = oldComponent.getElement();
-
-  if (!oldElement.parentElement || newElement === oldElement) {
-    return;
-  }
-
-  oldElement.parentElement.replaceChild(newElement, oldElement);
-}
-
-function remove(component) {
-  component.getElement().remove();
-  component.removeElement();
-}
-
-export {RenderPosition, createElement, render, replace, remove};
+export {RenderPosition, createElement, render};
